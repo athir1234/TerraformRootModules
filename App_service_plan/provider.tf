c7a26fbdae48f88d27876c7fdbdeddfb469221eb
athirson <athirson@yahoo.com>
2024-03-20 05:45:29 +0000

    backend1

--- App_service_plan/provider.tf
+++ App_service_plan/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "3.96.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "myfirstrg1"
-    storage_account_name = "myfirstsa09022024231900"
-    container_name       = "tfstate"
-    key                  = "terraform.module.tfstate"
-    }
+  # backend "azurerm" {
+  #   resource_group_name  = "myfirstrg1"
+  #   storage_account_name = "myfirstsa09022024231900"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.module.tfstate"
+  #   }
 }
 
 provider "azurerm" {
